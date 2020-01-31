912794b74fa2976a78405372c87881961c2264d2
Yobany <yobanydesign@gmail.com>
2020-01-31 09:19:23 -0500

    Added modal for add teams button. Firebase is now connected to site.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-var studentsReference = db.collection("students").doc("lq211zdSxBy1lxI4ibbk");
+function writeData() {
+    firebase.database().ref("User").set({
+         name: document.getElementById("nameField").value,
+         age: document.getElementById("ageField").value
+    })
+    getData();
+};
+
+function writeQuiz() {
+    firebase.database().ref("Quizzes").set({
+         redTeam: document.getElementById("rTeamName").value,
+         yellowTeam: document.getElementById("yTeamName").value
+    })
+};
+
+function getData() {
+     firebase.database().ref('/').once('value', function(snapshot) {
+         snapshot.forEach(function(childSnapshot)
+         {
+            var childKey =  childSnapshot.key;
+            var childData = childSnapshot.val();
+            document.getElementById("data").innerHTML = childData["name"] + ", " + childData["age"];
+         })
+     })
+};
